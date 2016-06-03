029b9073c8f09b807853bdcaa5d91a9654b4d45d
Jannis Mattheis <jmattheis@users.noreply.github.com>
2016-06-03 12:55:44 +0200

    add processmgmt

---
+++ processmgmt.sh
@@ -0,0 +1,52 @@
+﻿#!/bin/bash
+while true; do
+	clear
+	echo 
+	echo '             ____                                  '
+	echo '            / __ \_________  ____  ___  __________ '
+	echo '           / /_/ / ___/ __ \/_  / / _ \/ ___/ ___/ ' 
+	echo '          / ____/ /  / /_/ / / /_/  __(__  |__  )  '
+	echo '         /_/   /_/   \____/ /___/\___/____/____/   '                        
+	echo '                                     ____                   '
+	echo '     _   _____  ______      ______ _/ / /___  ______  ____ _'
+	echo '    | | / / _ \/ ___/ | /| / / __ `/ / __/ / / / __ \/ __ `/'
+	echo '    | |/ /  __/ /   | |/ |/ / /_/ / / /_/ /_/ / / / / /_/ / '
+	echo '    |___/\___/_/    |__/|__/\__,_/_/\__/\__,_/_/ /_/\__, /  '
+	echo '                                                   /____/   '
+	echo
+	echo
+	echo '  (1) Prozesse Anzeigen'
+	echo '   (2) Prozesse mit einem bestimmten Namen anzeigen'
+	echo '    (3) Prozess TÖTEN'
+	echo '     (x) exit'
+	echo 
+	echo 
+
+	read -n 1 selection
+	
+	case $selection in
+		1) 
+			ps aux
+			
+			echo ">>> Zurück zum Menü, irgenteine Taste drücken <<<"
+			read -n 1 uselessvariable
+			
+		;;
+		2)
+			echo "Welcher name soll der Prozess enthalten?"
+			read name
+			ps aux|grep $name
+			
+			echo ">>> Zurück zum Menü, irgenteine Taste drücken <<<"
+			read -n 1 uselessvariable
+		;;
+		3)
+			ps aux
+			echo
+			echo "Welcher Prozess soll getötet werden? (id)"
+			read process
+			kill -9 $process
+		;;
+		"x") exit; ;;
+	esac
+done
