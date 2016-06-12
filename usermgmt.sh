27cbcf3ae3e4acd11170ce135893e325f71b7132
Broder Peters <broder.peters@web.de>
2016-06-12 20:32:09 +0200

    adjust usermgmt

--- usermgmt.sh
+++ usermgmt.sh
@@ -19,28 +19,118 @@ function groupView {
 function userCreate {
 	echo 'Geben Sie den Nutzernamen ein:'
 	read username
-	echo "Möchten Sie ein Homeverzeichnis für $username erstellen? (J=Ja)"
-	read homedir
-	echo ${homedir^^}
-	if [ ${homedir^^} == "J" ]; then
-		useradd $username -m
+
+	if ! id "$username" >/dev/null 2>&1; then
+		echo "Möchten Sie ein Homeverzeichnis für $username erstellen? (J=Ja)"
+		read homedir
+		if [ ${homedir^^} == "J" ]; then
+			useradd $username -m
+		else
+			useradd $username
+		fi
+		passwd $username
 	else
-		useradd $username
+		echo 'Nutzer existiert bereits!'
 	fi
-	
+	read -n 1
+}
+
+function userChangeName {
+	echo "Geben Sie den neuen Nutzernamen für $1 ein:"
+	read newusername
+
+	usermod -l $newusername $1
+	read -n 1
+}
+
+function userChangePassword {
 	passwd $username
+	read -n 1	
+}
+
+function userChangeNewHome {
+	echo 'Geben Sie den Pfad für das neue Home-Verzeichnis ein:'
+	read directory
+	if [ -d "$directory" ]; then
+	echo 'Soll der Inhalt des aktuellen Home-Verzeichnisses in das neue Home-Verzeichnis verschoben werden? (J=Ja)'
+	read moveHome
+		if [ ${moveHome^^} == "J" ]; then
+			usermod -d $directory -m $1
+		else
+			usermod -d $directory $1
+		fi
+	fi
+	read -n 1
+}
+
+function userChangeMoveHome {
+	echo 'userChangeMoveHome'
+	read -n 1
 }
 
 function userChange {
-	echo 'userChange'
+	echo 'Geben Sie den aktuellen Nutzernamen ein:'
+	read username
+	
+	if id "$username" >/dev/null 2>&1; then
+		echo "Was möchten Sie für $username ändern?"
+		echo '  (1) Nutzername'
+		echo '   (2) Passwort'
+		echo '    (3) Neues Homeverzeichnis'
+		echo '     (4) Homeverzeichnis verschieben'
+		read -n 1 selection
+		echo -ne "\r" # remove input
+
+		case $selection in
+		1) userChangeName $username; ;;
+		2) userChangePassword $username; ;;
+		3) userChangeNewHome $username; ;;
+		4) userChangeMoveHome $username; ;;
+		esac
+	else
+		echo 'Nutzer existiert nicht!'
+	fi
+
+	read -n 1
 }
 
 function userDelete {
-	echo 'userDelete'
+	echo 'Geben Sie den Nutzernamen des zu löschenden Nutzers ein:'
+	read delusername
+
+	if id "$delusername" >/dev/null 2>&1; then
+	# Möchte man für Shell-Skripte nur den Rückgabewert eines Programms verwenden und somit 	  sämtliche Ausgaben nach /dev/null umleiten, kann die folgende Syntax verwendet werden: 		  >/dev/null 2>&1
+		echo 'Möchten Sie das Home-Verzeichnis und den Mailspool entfernen? (J=Ja)'
+		read delhome
+		if [ ${delhome^^} == "J" ]; then
+			userdel -r $delusername
+		else
+			userdel $delusername
+		fi
+		
+	else
+		echo 'Nutzer existiert nicht!'
+	fi
+
+	read -n 1
 }
 
 function groupCreate {
-	echo 'groupCreate'
+	echo 'Geben Sie den Gruppennamen ein:'
+	read groupname
+
+	if ! grep ^$groupname: /etc/group >/dev/null 2>&1; then
+		echo 'Möchten Sie ein Passwort für die Gruppe festlegen? (J=Ja)'
+		read password
+
+		groupadd $groupname
+		if [ ${password^^} == "J" ]; then
+			gpasswd $groupname
+		fi
+	else
+		echo 'Gruppe existiert bereits!'
+	fi
+	read -n 1
 }
 
 function groupChange {
@@ -91,9 +181,9 @@ while true; do
 	echo '          (9) Benutzer einer Gruppe zuordnen'
 	echo '           (a) Benutzer aus einer Gruppe entfernen'
 	echo '            (x) Hauptmenü'
-
+	title
 	read -n 1 selection
-    echo -ne "\r" # remove input
+	echo -ne "\r" # remove input
 	
 	case $selection in
 		1) userView; ;;
